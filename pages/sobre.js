6e409266569cb5ea913c8d9a009a8a2c81605ca7
NaughtyDOGS <vitcimgirls12@hotmail.com>
2021-03-25 09:36:59 -0300

    nova pagina

---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+function Sobre(){
+    return(
+    <div>
+        <h1>em Progreço by naughty</h1>
+     <link>
+      <link href="/">
+       <a>Assessa pagina Home</a>
+      </link>
+     </link>
+    </div>
+    )
+}
+export default Sobre
