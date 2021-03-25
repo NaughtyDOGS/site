e866b013f0d422b8b5aef22feb5762fb6ae6e3f2
NaughtyDOGS <vitcimgirls12@hotmail.com>
2021-03-25 20:00:35 -0300

    NovaPagina

--- pages/strnger.js
+++ pages/strnger.js
@@ -4,7 +4,9 @@ function Strnger(){
     return(
     <div>
         <h1>O Virtual Riot Paulista jkkk</h1>
-             <a>Acessar pagina do Naughty</a>
+        <Link href="/Vinison">
+            <a>Acessar pagina do vinison</a>
+        </Link>
     </div>
     )
 }
---
+++ pages/vinison.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+function Vinison(){
+    return(
+    <div>
+        <h1>O sound disnger brasiliano</h1>
+        <Link href="/Strnger">
+            <a>Acessar pagina do strnger</a>
+        </Link>
+    </div>
+    )
+}
+export default Vinison
